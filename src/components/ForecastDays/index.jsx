a938aa7639b38f83f898697509570c278c1c36d3
Irena Tagesyan <irenatagesyan@gmail.com>
2024-04-04 11:50:22 +0400

    feat: Createing new file for constants and slice forecast weather array

--- src/components/ForecastDays/index.jsx
+++ src/components/ForecastDays/index.jsx
@@ -1,23 +1,9 @@
 import React from "react";
 import styles from "./forecast.module.scss";
+import { currentDay, weekDays } from "./constants";
 
 export const ForecastDays = ({ forecastDaysData }) => {
-  const forecastDaysWeatherArray = [
-    forecastDaysData.list[0],
-    forecastDaysData.list[1],
-    forecastDaysData.list[2],
-    forecastDaysData.list[3],
-  ];
-  const currentDay = new Date().getDay();
-  const weekDays = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ];
+  const forecastDaysWeatherArray = forecastDaysData.list.slice(0, 4);
 
   const forecastDays = weekDays
     .slice(currentDay, weekDays.length)
